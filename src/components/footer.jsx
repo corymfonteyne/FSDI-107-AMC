6149b7a19850be9e9dd8f4a790df48e75e0acd87
corymfonteyne <corymfonteyne@gmail.com>
2022-08-03 20:21:45 -0400

    progress

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+
+import './footer.css';
+
+function Footer(){
+    return (
+        <div className="footer">
+            <h5>Arbor Mill Customs &copy; 2022</h5>
+        </div>
+    )
+}
+
+
+export default Footer;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,12 @@
+
+
+function NavBar(){
+    return (
+        <div className="NavBar">
+            <h5>Navbar will be here later!!</h5>
+        </div>
+    )
+}
+
+
+export default NavBar;
